5f7ebc07d045c23c153476c92369834ef9ec7233
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:46:45 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class WaibuBootstrapIcons extends this.lib.BajoPlugin {
+  return class WaibuBootstrapIcons extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'wbsi'
